0ffec132f1649f0592167f74ecf242773d1d35b0
EricWithC04 <negativegame117@gmail.com>
2024-05-28 21:37:53 -0300

    feat: 2 more list of test

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,6 @@
 numeros = [17, 2, 1, 8, 5, 3, 6, 3, 2, 4, 5, 3, 4, 6, 75, 5, 2, 42, 4, 23]
+numeros2 = [12, 20, 3, 8, 19, 1, 23, 7, 14, 16, 21, 6, 5, 24, 10, 4, 25, 18, 21]
+numeros3 = [17, 3, 22, 11, 19, 6, 25, 9, 2, 20, 5, 12, 24, 4, 15, 13, 10, 8, 21, 7]
 
 def partition(arr, min, max):
 	pivot = arr[max]
@@ -26,3 +28,11 @@ def quickSort(arr, min, max):
 print(numeros)
 quickSort(numeros, 0, len(numeros) - 1)
 print(numeros)
+
+print(numeros2)
+quickSort(numeros2, 0, len(numeros2) - 1)
+print(numeros2)
+
+print(numeros3)
+quickSort(numeros3, 0, len(numeros3) - 1)
+print(numeros3)
